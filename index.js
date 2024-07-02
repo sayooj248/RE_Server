ee3fe1e7762833eca1c0a9d53040efc5468b9479
sayooj248 <151500571+sayooj248@users.noreply.github.com>
2024-07-02 15:20:30 +0530

    Update index.js

--- index.js
+++ index.js
@@ -24,6 +24,12 @@ mongoose
 
   const PORT =3000 || process.env.PORT
 
+ app.get('/', (req, res) => {
+    res.send('Hello World!')
+  })
+
+
+
 
 app.listen(PORT, () => {
   console.log(`Server is running on port:${PORT}`);
